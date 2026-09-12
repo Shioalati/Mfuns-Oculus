a5b4206e5328e0524ae547cebd212783ffc1888f
Noel-333 <17730622187@163.com>
2026-09-12 22:29:27 +0800

    chore: 配置文件云云

---
+++ src/index.js
@@ -0,0 +1,11 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/api/user-count") {
+      return Response.json({ count: 0 });
+    }
+
+    return env.ASSETS.fetch(request);
+  }
+};
